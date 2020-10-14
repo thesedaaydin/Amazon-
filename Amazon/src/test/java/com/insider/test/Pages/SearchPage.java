b4986812ddc7ad7dbce7581a97d1c1fe16e54ec3
thesedaaydin <69966734+thesedaaydin@users.noreply.github.com>
2020-10-14 17:36:45 +0300

    Update SearchPage.java

--- Amazon/src/test/java/com/insider/test/Pages/SearchPage.java
+++ Amazon/src/test/java/com/insider/test/Pages/SearchPage.java
@@ -9,7 +9,9 @@ public class SearchPage extends BasePage {
     public SearchPage(WebDriver driver) {
         super (driver);
     }
-
+    
+// Search işlemleri , product=aranacak olan kelime, pageCount=gitmek istenilen sayfa sayısı, productCount= seçilmek istenilen ürünün sırası(0'dan başlıyor.)
+ 
     public SearchPage search(String product,String pageCount, String productCount) throws InterruptedException {
         setId ("twotabsearchtextbox", product);
         clickId ("nav-search-submit-text");
@@ -18,6 +20,7 @@ public class SearchPage extends BasePage {
         TimeUnit.SECONDS.sleep (2);
         clickCssSelector ("div[data-cel-widget='search_result_"+productCount+"']");
         TimeUnit.SECONDS.sleep (2);
+         //  Search yaptıktan sonra  seçilen ürünü silenen ürünle karşılaştırmak için konsola yazdırdım.
         System.out.println("Listeye product sayfasından eklenen ürün");
         write ("div[id='titleSection']>h1>span");
         return new SearchPage (driver);
